aa6190530c8d211377b9d024fe3d644613b5b9d8
Ne184 <nevena.gincheva@abv.bg>
2021-08-06 21:24:43 +0200

    Chef implementation

--- src/main/kotlin/services/ChefService.kt
+++ src/main/kotlin/services/ChefService.kt
@@ -1,15 +1,36 @@
 package services
 
 import database.entities.Chef
-import database.entities.Post
+import database.entities.Chef.score
 import database.synchronizeMemoryDB
 import dtos.ChefDTO
 
 class ChefService {
 
     fun getChefs(): List<ChefDTO> {
-        val posts = synchronizeMemoryDB(Post)
-        return posts.map { ChefDTO(name = it[Chef.name], score = it[Chef.score]) }
+        val chefs = synchronizeMemoryDB(Chef)
+        return chefs.map { ChefDTO(id = it[Chef.id].value, name = it[Chef.name], score = it[Chef.score]) }
     }
+
+    fun getChefById(idParam:Int): ChefDTO {
+        val chefs = getChefs()
+        val chefsId = chefs.filter{it.id == idParam}
+        if(chefsId.size > 1) {
+            //error
+        }
+        return chefsId.last()
+    }
+
+    fun getChefScore(idParam: Int):Int {
+        val chef = getChefById(idParam)
+        return chef.score;
+    }
+
+//    fun getChefsHighestScore(number: Int):List<ChefDTO>{
+//        val chefs = getChefs()
+//        chefs.sortedWith(ChefDTO)
+//        return
+//    }
+
 
 }
