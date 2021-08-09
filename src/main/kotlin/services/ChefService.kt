921153f8bb041b1ee613c21faa99b50af6e6673a
Ne184 <nevena.gincheva@abv.bg>
2021-08-09 11:37:19 +0200

    Finished last fun and added some comments

--- src/main/kotlin/services/ChefService.kt
+++ src/main/kotlin/services/ChefService.kt
@@ -7,30 +7,50 @@ import dtos.ChefDTO
 
 class ChefService {
 
+    /**
+     * Retrieves all chefs in from the database.
+     * @return a list with all chefs
+     */
     fun getChefs(): List<ChefDTO> {
         val chefs = synchronizeMemoryDB(Chef)
         return chefs.map { ChefDTO(id = it[Chef.id].value, name = it[Chef.name], score = it[Chef.score]) }
     }
 
-    fun getChefById(idParam:Int): ChefDTO {
+    /**
+     * Searches for a chef by an id.
+     * @param idParam the id of the chef to be return
+     * @return the chef with the given id if exists else null
+     */
+    fun getChefById(idParam:Int): ChefDTO? {
         val chefs = getChefs()
-        val chefsId = chefs.filter{it.id == idParam}
+        val chefsId = chefs.filter{ it.id == idParam }
         if(chefsId.size > 1) {
-            //error
+            //error, more chefs with the same id, possible?
         }
-        return chefsId.last()
+        return if (chefsId.size == 0) null else chefsId.last()
     }
 
-    fun getChefScore(idParam: Int):Int {
+    /**
+     * Search for the chef's score by a given id.
+     * @param idParam the id of the chef
+     * @return the chef's score if a chef with such id exists
+     * else null
+     */
+    fun getChefScore(idParam: Int):Int? {
         val chef = getChefById(idParam)
-        return chef.score;
+        return chef?.score
     }
 
-//    fun getChefsHighestScore(number: Int):List<ChefDTO>{
-//        val chefs = getChefs()
-//        chefs.sortedWith(ChefDTO)
-//        return
-//    }
+    /**
+     * Retrieves a given number of chefs with the highest score.
+     * @param number the number of the chefs to be return
+     * @return a list of chefs with the highest score
+     */
+    fun getChefsHighestScore(number: Int):List<ChefDTO>{
+        val chefs = getChefs()
+        chefs.sortedWith( compareByDescending { it.score })
+        return chefs.slice(IntRange(0,number - 1))
+    }
 
 
 }
